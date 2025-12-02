d8e41fd5d97452de10b9a2b2601c355600816986
Maxime Bardouil <maxime.bardouil10@gmail.com>
2025-12-02 17:44:29 +0100

    first version: data_download.py

--- scripts/data_download.py
+++ scripts/data_download.py
@@ -1,2 +1,32 @@
 import requests
+import xarray as xr
+from io import BytesIO
+import os
 
+def data_download(url):
+    if url == "":
+        url = "https://object.files.data.gouv.fr/meteofrance-drias/SocleM-Climat-2025/EMULATEUR/METROPOLE/ALPX-12/MPI-ESM1-2-LR/r1i1p1f1/CNRM-ALADIN63-emul-CNRM-UNET11-tP22/historical/day/prAdjust/version-hackathon-102025/prAdjust_FR-Metro_MPI-ESM1-2-LR_historical_r1i1p1f1_CNRM_CNRM-ALADIN63-emul-CNRM-UNET11-tP22_v1-r1_MF-CDFt-SAFRAN-1985-2014_day_18500101-20141231.nc"
+
+    filename = "./data_perso/" + os.path.basename(url)
+
+    if not os.path.exists(filename):
+
+        print("download_script.py : File not found, fetching data from online.")
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        with open(filename, "wb") as f:
+            f.write(response.content)
+    else:
+        print("download_script.py : File already exists.")
+
+    print("download_script.py : File size: ", os.path.getsize(filename))
+    print("download_script.py : Crrating dataset.")
+    ds = xr.open_dataset(filename)
+
+    return ds
+
+#arr = ds.to_array().values
+#print(type(arr))
+#print(arr.shape)
